251f9ede1c72676aeeb3b71bd576b2c41a8bb15f
Tyler Brown <tyler@sliceofnet.com>
2017-03-16 19:51:41 -0400

    Removed code for testing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   # resources :users
-  get '/users/:id/edit', to: 'users#edit'
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
+
 end
