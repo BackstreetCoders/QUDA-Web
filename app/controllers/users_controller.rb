7ab06d64e578977f121dc7823e30f1cc409b62e2
Birstinger <ethanmseser14@gmail.com>
2017-03-16 20:26:54 -0400

    mostly useless nonsense, learning
    ^

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render("users/index")
+    # or:  " render :index "
+  end
+
+  def display_all
+    @users = User.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   # resources :users
+  get '/users', to: 'users#index'
+  get '/users/display_all', to: 'users#display_all'
+
 
 end
